3dbf20bd86bfba3680a4dee13d43ab3db4e7ee53
[CST] Focus <focus.cst@gmail.com>
2012-09-25 15:16:38 +0400

    build.sh: uncomment make modules

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make -j8 ARCH=arm CROSS_COMPILE=arm-cx2450x-linux-gnueabi- zImage
-#make -j8 ARCH=arm CROSS_COMPILE=arm-cx2450x-linux-gnueabi- modules
+make -j8 ARCH=arm CROSS_COMPILE=arm-cx2450x-linux-gnueabi- modules
 ./mkimage -A arm -O linux -T kernel -C none -a  0x48000 -e  0x48000 -n "Coolstream HDx Kernel" -d arch/arm/boot/zImage uImage
