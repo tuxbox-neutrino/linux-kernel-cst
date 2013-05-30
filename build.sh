586e1e89af11f21eb60cab8f4550393ed0b682b9
[CST] Focus <focus.cst@gmail.com>
2013-05-30 13:11:54 +0400

    build.sh: use zImage

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 make -j4 ARCH=arm CROSS_COMPILE=arm-cx2450x-linux-gnueabi- zImage
 make -j4 ARCH=arm CROSS_COMPILE=arm-cx2450x-linux-gnueabi- modules
-./mkimage -A arm -O linux -T kernel -C none -a 0x17048000 -e 0x17048000 -n "Coolstream HDx Kernel" -d arch/arm/boot/Image uImage
+./mkimage -A arm -O linux -T kernel -C none -a 0x17048000 -e 0x17048000 -n "Coolstream HDx Kernel" -d arch/arm/boot/zImage uImage
