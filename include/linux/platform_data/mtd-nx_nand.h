4b6d26a269db28b8e1e602ce2c211c1f4c71354e
[CST] Bas <bas@coolstreamtech.com>
2015-04-11 13:08:59 +0800

    CST: add NAND device timing to DT.

---
+++ include/linux/platform_data/mtd-nx_nand.h
@@ -0,0 +1,19 @@
+/*
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef __PLAT_NX_NAND_H
+#define __PLAT_NX_NAND_H
+
+/*
+ * Device bus NAND private data
+ */
+struct nx_nand_pdata {
+	u32	dev_timing0;
+	u32	dev_timing1;
+};
+
+#endif /* __PLAT_NX_NAND_H */
